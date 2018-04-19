51fa74a2847d7fecf8a4100dfbf9a6658cf6ff09
m <m@m-PC>
2018-04-19 09:28:31 +0800

    fixed "sping_web_xml_config_demo" to "spring_web_xml_config_demo"

---
+++ spring_web_xml_config_demo/src/main/java/sping_web_xml_config_demo/entity/UserT.java
@@ -0,0 +1,30 @@
+package spring_web_xml_config_demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="use_t")
+public class UserT {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getId() {
+		return id;
+	}
+	
+	
+	
+}
---
+++ spring_web_xml_config_demo/src/test/java/sping_web_xml_config_demo/ApplicationContextTest.java
@@ -0,0 +1,18 @@
+package spring_web_xml_config_demo;
+
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ApplicationContextTest {
+
+	
+	public static void main(String... args) {
+		
+		ApplicationContext ac=new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		
+	}
+	
+}
